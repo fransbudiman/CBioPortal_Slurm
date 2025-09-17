26b75201b989db4f35931dacdd3b927b967d1249
Frans Budiman <fransrewl@gmail.com>
2025-09-17 16:48:56 -0400

    typo

--- bin/driver.sh
+++ bin/driver.sh
@@ -58,4 +58,4 @@ echo "Job ID for VEP: $jid_vep"
 jid_vcf2maf=$($BIN_DIR/vcf2maf.sh -i "$VEP_DIR" -p "$STUDY_ID" -r "$REF_DIR/hg19.fa.gz.fai" -D "$jid_vep" | awk '/jid:/ {print $2}')
 echo "Job ID for VCF2MAF: $jid_vcf2maf"
 
-./$BIN_DIR/create_study.sh -i "$STUDY_ID" -n "$STUDY_NAME" -d "$STUDY_DESC" -m "$MAF_DIR" -t "$TSV_FILE" -D "$jid_vcf2maf"
+$BIN_DIR/create_study.sh -i "$STUDY_ID" -n "$STUDY_NAME" -d "$STUDY_DESC" -m "$MAF_DIR" -t "$TSV_FILE" -D "$jid_vcf2maf"
