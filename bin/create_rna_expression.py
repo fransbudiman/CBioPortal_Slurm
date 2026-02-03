9926e2d46687b9a5b258265d795cba05cda94da3
Frans Budiman <fransrewl@gmail.com>
2026-02-03 17:07:41 -0500

    implement rna exp files maker

--- bin/create_rna_expression.py
+++ bin/create_rna_expression.py
@@ -33,3 +33,41 @@ if not os.path.exists(sample_info_path):
 
 # create metadata file for RNA data
 metadata_output_path = output_directory / 'meta_expression.txt'
+
+metadata_lines = [
+    f"cancer_study_identifier: {study_id}",
+    "genetic_alteration_type: MRNA_EXPRESSION",
+    "datatype: CONTINUOUS",
+    "stable_id: rna_seq_mrna",
+    "show_profile_in_analysis_tab: false",
+    "profile_name: mRNA expression",
+    "profile_description: Expression levels",
+    "data_filename: data_expression.txt"
+]
+with open(metadata_output_path, 'w') as meta_file:
+    meta_file.write("\n".join(metadata_lines))
+print(f"RNA expression metadata file created at: {metadata_output_path}")
+
+
+# create data_expression.txt
+
+# outer join and loop through all txt files in rna_dir
+expression_df = pd.DataFrame()
+
+for file in os.listdir(rna_dir):
+    if file.endswith('.txt'):
+        file_path = os.path.join(rna_dir, file)
+        sample_df = pd.read_csv(file_path, sep="\t", index_col=0)
+        expression_df = pd.concat([expression_df, sample_df], axis=1, join='outer')
+
+# Rename gene column to 'Hugo_Symbol'
+expression_df.index.name = 'Hugo_Symbol'
+# Fill NaN with 'NA'
+expression_df.fillna('NA', inplace=True)
+
+# Write to data_expression.txt
+data_expression_path = output_directory / 'data_expression.txt'
+expression_df.to_csv(data_expression_path, sep="\t")
+print(f"RNA expression data file created at: {data_expression_path}")
+
+        
